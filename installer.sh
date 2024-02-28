26356d92484624fde1495fbf17769a352c448bf2
Ari <134935244+AriTheFirst@users.noreply.github.com>
2024-02-27 20:51:34 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,4 +3,5 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 sh -c "$(https://raw.githubusercontent.com/AriTheFirst/BreezeEnhancedCatppuccin/main/installer.sh)"
+curl https://raw.githubusercontent.com/AriTheFirst/kde-backup/main/CatppuccinMochaMauve.colors -O ~/.local/share/color-schemes/CatppuccinMochaMauve.colors
 yay -S vesktop
